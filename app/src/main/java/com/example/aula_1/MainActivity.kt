4d5987775ad2b66268b406d0c529ddc6159b528c
Marco <marcoalsampaio@gmail.com>
2022-02-25 11:54:02 +0000

    Initial commit (all files)

---
+++ app/src/main/java/com/example/aula_1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.aula_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+//Integração Github com o android stdio
